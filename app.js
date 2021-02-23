11ff7898ea32b5d2bef5640847c59e21d5a4ad69
indrakumarpathak <indrapathak.osl@gmail.com>
2021-02-23 11:22:50 +0530

    changes

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const localIP = 'localhost';
 // });
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://angular-10-project.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://angular-10-project.herokuapp.com');
     //res.setHeader('Access-Control-Allow-Origin', 'http://'+localIP+':3000, http://localhost')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
